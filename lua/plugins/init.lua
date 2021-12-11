912184a4bd3225ee3237c855fa0b2fdb0eb44202
Akhil Robert <akhilrobert2412@gmail.com>
2021-12-11 17:26:51 +0530

    closes #5

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -66,4 +66,6 @@ packer.startup(function(use)
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
+
+	use("jose-elias-alvarez/nvim-lsp-ts-utils")
 end)
