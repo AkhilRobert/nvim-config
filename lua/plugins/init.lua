3924ffb96edab2dbc156fce0584c35cbcf39d2ad
Akhil Robert <akhilrobert2412@gmail.com>
2021-12-11 16:23:24 +0530

    closes #7

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,7 +67,7 @@ packer.startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true}
     }
-    use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+    -- use {'romgrk/barbar.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
 
 
 end)
--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -1,4 +1,4 @@
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 local formatter = null_ls.builtins.formatting
 
 local sources = {
@@ -6,16 +6,18 @@ local sources = {
 
 	-- python
 	formatter.black,
-	formatter.isort
+	formatter.isort,
+
+	-- lua
+	formatter.stylua,
 }
 
-null_ls.config({sources = sources})
+null_ls.config({ sources = sources })
 
-require("lspconfig")['null-ls'].setup {
-	on_attach = function (client)
+require("lspconfig")["null-ls"].setup({
+	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
 			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
 		end
-	end
-}
-
+	end,
+})
