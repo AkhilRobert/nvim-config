5febe5ca9ed9ca5e10a650218b88f190c4e4a522
Akhil Robert <akhilrobert2412@gmail.com>
2021-12-14 20:59:29 +0530

    Fixes null-ls depreceation warning

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -1,5 +1,4 @@
 local null_ls = require("null-ls")
-local lspconfig = require("lspconfig")
 local formatter = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
@@ -29,8 +28,8 @@ local sources = {
 }
 
 M.setup = function()
-	null_ls.config({ sources = sources })
-	lspconfig["null-ls"].setup({
+	null_ls.setup({
+		sources = sources,
 		on_attach = function(client)
 			if client.resolved_capabilities.document_formatting then
 				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
