00faade41be73897010a76f13c42bdde771beaa6
Akhil Robert <akhilrobert2412@gmail.com>
2022-07-14 12:42:17 +0530

    Added comment string for prisma

--- lua/lsp/servers/prisma.lua
+++ lua/lsp/servers/prisma.lua
@@ -1,5 +1,9 @@
 local lsp = require('lspconfig')
+local ft = require('Comment.ft')
 local M = {}
+
+-- set's up comment string for prisma
+ft.set('prisma', '//%s')
 
 M.setup = function()
 	lsp.prismals.setup {}
