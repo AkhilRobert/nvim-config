ed47f0c0395de26f833fd7899b4c5a58bde4bd8c
Akhil Robert <akhilrobert2412@gmail.com>
2023-06-13 11:32:56 +0530

    fix: fidget legacy warning

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,6 +1,8 @@
+-- TODO: move from legacy branch once migration is done
 return {
   "j-hui/fidget.nvim",
   event = "LspAttach",
+  branch = "legacy",
   config = function()
     require('fidget').setup()
   end
