6a951290aa6c5c0396241fda0f861ba78960b9e4
Akhil Robert <akhilrobert2412@gmail.com>
2023-10-19 00:47:24 +0530

    Added java support

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,30 +1,30 @@
-local c = require('vscode.colors').get_colors()
-local vscode = require('vscode')
-
+-- local c = require('vscode.colors').get_colors()
+-- local vscode = require('vscode')
+--
 -- local hl = vim.api.nvim_set_hl
-vim.o.background = "dark"
-
-local c_constant = "#4FC0FF"
-
-vscode.setup({
-  group_overrides = {
-    ['@constant'] = { fg = c_constant, bg = 'NONE' },
-    DiagnosticUnnecessary = { underline = true },
-    ['@text.title'] = {},
-    ['@text.title.1'] = { fg = c.vscBlue, bold = true },
-    ['@text.title.2'] = { fg = c.vscBlue, bold = true },
-    ['@lsp.typemod.variable.constant.rust'] = { fg = c_constant },
-    ['@lsp.typemod.variable.readonly.typescriptreact'] = { fg = c_constant },
-    ['@lsp.typemod.variable.readonly.go'] = { fg = c_constant },
-  }
-})
-vscode.load()
-
-
--- require('tokyonight').setup({
---   styles = {
---     keywords = { italic = false }
+-- vim.o.background = "dark"
+--
+-- local c_constant = "#4FC0FF"
+--
+-- vscode.setup({
+--   group_overrides = {
+--     ['@constant'] = { fg = c_constant, bg = 'NONE' },
+--     DiagnosticUnnecessary = { underline = true },
+--     ['@text.title'] = {},
+--     ['@text.title.1'] = { fg = c.vscBlue, bold = true },
+--     ['@text.title.2'] = { fg = c.vscBlue, bold = true },
+--     ['@lsp.typemod.variable.constant.rust'] = { fg = c_constant },
+--     ['@lsp.typemod.variable.readonly.typescriptreact'] = { fg = c_constant },
+--     ['@lsp.typemod.variable.readonly.go'] = { fg = c_constant },
 --   }
 -- })
---
--- vim.cmd [[colorscheme tokyonight]]
+-- vscode.load()
+
+
+require('tokyonight').setup({
+  styles = {
+    keywords = { italic = false }
+  }
+})
+
+vim.cmd [[colorscheme tokyonight]]
